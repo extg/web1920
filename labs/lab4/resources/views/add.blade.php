4d25d88550df6c12b5581098f1381027dbd0f69e
extg <extg@yandex-team.ru>
2019-03-16 17:05:20 +0300

    Validations

--- labs/lab4/resources/views/add.blade.php
+++ labs/lab4/resources/views/add.blade.php
@@ -11,19 +11,31 @@
                 </h2>
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title">
+                    <input type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" id="title" name="title" placeholder="Title" value="{{ old('title') }}">
+                    @if($errors->has('title'))
+                    <span class="help-block">{{ $errors->first('title') }}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="slug">Slug</label>
-                    <input type="text" class="form-control" id="slug" name="slug">
+                    <input type="text" class="form-control{{ $errors->has('slug') ? ' is-invalid' : '' }}" id="slug" name="slug">
+                    @if($errors->has('slug'))
+                    <span class="help-block">{{ $errors->first('slug') }}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="description">Short description</label>
-                    <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                    <textarea class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" id="description" rows="3" name="description"></textarea>
+                    @if($errors->has('description'))
+                    <span class="help-block">{{ $errors->first('description') }}</span>
+                    @endif
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <textarea class="form-control" id="content" rows="3" name="content"></textarea>
+                    <textarea class="form-control{{ $errors->has('content') ? ' is-invalid' : '' }}" id="content" rows="3" name="content"></textarea>
+                    @if($errors->has('content'))
+                    <span class="help-block">{{ $errors->first('content') }}</span>
+                    @endif
                 </div>
                 <button class="btn btn-success">Submit</button>
             </form>
